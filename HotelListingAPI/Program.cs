aaa0381e7415bb4b67fddfa4447312e3ec10f713
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2023-08-25 22:32:50 +0800

    Adding Serilog from Nuget and setting up the Serilog in the Program.cs

--- HotelListingAPI/Program.cs
+++ HotelListingAPI/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,6 +16,8 @@ builder.Services.AddCors(
                 .AllowAnyOrigin()
                 .AllowAnyMethod());
     });
+//Lesson 2 : Add Serilog 
+builder.Host.UseSerilog((ctx,lc)=>lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 
 var app = builder.Build();
 
